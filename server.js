a2551b50e4775f4a548e347cc586d844a71d98e7
taivic <vicky.yen.tai@gmail.com>
2017-02-07 09:05:02 -0600

    deleted router.js

--- server.js
+++ server.js
@@ -1,20 +1,68 @@
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const blogPostRouter = require ('./blogPostRouter')
 const app = express();
 const jsonParser = bodyParser.json();
-
 const {BlogPosts} = require('./model');
-
 app.use(morgan('common'));
 
-app.use('/blog-posts', blogPostRouter)
+BlogPosts.create('How to stay active', 'It is a mental and physical thing', 'Vic Tai', 'February 2nd, 2017');
+BlogPosts.create('How to scare off intruders', 'Bark and Bark at everything', 'Marley and Honey', 'March 1st, 2016')
+BlogPosts.create('How to make the best Mac-and-cheese', 'The secret is eggs', 'Olynthia', 'January 15th, 1984');
 
-// both runServer and closeServer need to access the same
-// server object, so we declare `server` here, and then when
-// runServer runs, it assigns a value.
+app.get('/blog-posts', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+app.post('/blog-posts', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+  const field = requiredFields[i];
+    if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+    }
+  }
+    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.status(201).json(item);
+});
+
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+          const message = `Missing \`${field}\` in request body`
+          console.error(message);
+          return res.status(400).send(message);
+      }
+    }
+    if (req.params.id !== req.body.id) {
+      const message = (
+        `Request path id (${req.params.id}) and request body id `
+        `(${req.body.id}) must match`);
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    console.log(`Updating blog posts item \`${req.params.id}\``);
+    const updatedItem = BlogPosts.update({
+      id: req.params.id,
+      name: req.body.name,
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      publishDate: req.body.publishDate
+    });
+    res.status(204).json(updatedItem);
+});
+
+app.delete('/blog-posts/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+    console.log(`Deleted Blog Post item \`${req.params.ID}\``);
+    res.status(204).end();
+});
+
 let server;
 
 function runServer() {
@@ -29,9 +77,6 @@ function runServer() {
   });
 }
 
-// like `runServer`, this function also needs to return a promise.
-// `server.close` does not return a promise on its own, so we manually
-// create one.
 function closeServer() {
   return new Promise((resolve, reject) => {
     console.log('Closing server');
@@ -45,8 +90,7 @@ function closeServer() {
     });
   });
 }
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+
 if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
